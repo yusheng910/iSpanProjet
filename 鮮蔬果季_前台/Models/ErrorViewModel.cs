e6dc22143eedd7026462ecb0931634eddc0d8aae
Franklyn0602 <95435270+Franklyn0602@users.noreply.github.com>
2021-12-05 10:14:50 +0800

    加入專案檔案。

---
+++ 鮮蔬果季_前台/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace 鮮蔬果季_前台.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ 鮮蔬果季_前台/Program.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace 鮮蔬果季_前台
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
