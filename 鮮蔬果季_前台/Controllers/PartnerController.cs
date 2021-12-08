9906416b220f8c6508850aa86b2c881c97d6cd85
dryadl978188 <dryadl978188@gmail.com>
2021-12-08 11:55:37 +0800

    12/8 terry 新增部落格及連結

---
+++ 鮮蔬果季_前台/Controllers/PartnerController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace 鮮蔬果季_前台.Controllers
+{
+    public class PartnerController : Controller
+    {
+        public IActionResult PartnerBlog()
+        {
+            return View();
+        }
+
+        public IActionResult PartnerBlog_1()
+        {
+            return View();
+        }
+
+    }
+}
