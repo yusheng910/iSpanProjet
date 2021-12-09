56074e2e0d775df9cf88edbb0857c375b6192aae
Franklyn0602 <95435270+Franklyn0602@users.noreply.github.com>
2021-12-09 18:49:24 +0800

    新增後台獨立套版_12/9 06:49

---
+++ 鮮蔬果季_前台/Controllers/BackstageController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace 鮮蔬果季_前台.Controllers
+{
+    public class BackstageController : Controller
+    {
+        public IActionResult Home()
+        {
+            return View();
+        }
+    }
+}
