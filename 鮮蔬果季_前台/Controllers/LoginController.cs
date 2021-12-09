88f3d77af2514e589d9c2e4c980573c788aaefb5
MinHao-Hsu <95455782+MinHao-Hsu@users.noreply.github.com>
2021-12-09 23:00:39 +0800

    更換會員中心位置

--- 鮮蔬果季_前台/Controllers/LoginController.cs
+++ 鮮蔬果季_前台/Controllers/LoginController.cs
@@ -16,9 +16,6 @@ namespace 鮮蔬果季_前台.Controllers
         {
             return View();
         }
-        public IActionResult MemberCenter()
-        {
-            return View();
-        }
+        
     }
 }
