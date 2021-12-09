44a4b0bb5bbb6591f2bbf94a3fc6c1f4543bc03d
yusheng910 <humanist910@gmail.com>
2021-12-09 19:39:19 +0800

    新增 MemberController

---
+++ 鮮蔬果季_前台/Controllers/MemberController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace 鮮蔬果季_前台.Controllers
+{
+    public class MemberController : Controller
+    {
+        public IActionResult Orders()
+        {
+            return View();
+        }
+        public IActionResult OrderDetail()
+        {
+            return View();
+        }
+    }
+}
