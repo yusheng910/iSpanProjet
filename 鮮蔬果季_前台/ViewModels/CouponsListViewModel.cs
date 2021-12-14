2ad1236195a53f01e1bf4aebbd73b0e26ea3c315
LeonLi0317 <isu10122006m@cloud.isu.edu.tw>
2021-12-14 13:05:02 +0800

    更動: OrderViewModel;Coupons增加join CouponsDetails

---
+++ 鮮蔬果季_前台/ViewModels/CouponsListViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using 鮮蔬果季_前台.Models;
+
+namespace 鮮蔬果季_前台.ViewModels
+{
+    public class CouponsListViewModel
+    {
+        public Coupon _coupon = null;
+        public Coupon coupon
+        {
+            get
+            {
+                if (_coupon == null)
+                    _coupon = new Coupon();
+                return _coupon;
+            }
+            set
+            {
+                _coupon = value;
+            }
+        }
+        public CouponDetail _couponDetail  = null;
+        public CouponDetail couponDetail
+        {
+            get
+            {
+                if (_couponDetail == null)
+                    _couponDetail = new CouponDetail();
+                return _couponDetail;
+            }
+            set
+            {
+                _couponDetail = value;
+            }
+        }
+    }
+}
