ede057972f0d8649f75a1bc5833069863f23d09d
Franklyn0602 <95435270+Franklyn0602@users.noreply.github.com>
2021-12-14 19:49:35 +0800

    CouponsList 更新回來

--- 鮮蔬果季_前台/Controllers/MemberController.cs
+++ 鮮蔬果季_前台/Controllers/MemberController.cs
@@ -48,24 +48,31 @@ namespace 鮮蔬果季_前台.Controllers
         public IActionResult CouponsList()
         {
             鮮蔬果季Context db = new 鮮蔬果季Context();
-            var q = from p in db.Coupons
-                    join x in db.CouponDetails
-                    on p.CouponId equals x.CouponId
-                    where x.MemberId == 2
-                    select new { x, p };
-            List<CouponsListViewModel> list = new List<CouponsListViewModel>(); 
-            foreach(var item in q)
+            var qall = from p in db.Coupons
+                       select p;
+            //var q = from p in db.Coupons
+            //        join x in db.CouponDetails
+            //        on p.CouponId equals x.CouponId
+            //        where x.MemberId == 2
+            //        select new { x, p };
+            List<CouponsListViewModel> list = new List<CouponsListViewModel>();
+            foreach (var item in qall)
             {
+                db = new 鮮蔬果季Context();
+                var q = (from cd in db.CouponDetails
+                         where cd.CouponId == item.CouponId
+                         select cd).FirstOrDefault();
                 list.Add(new CouponsListViewModel()
                 {
-                    coupon = item.p,
-                    couponDetail = item.x
+                    coupon = item,
+                    couponDetail = q
                 });
             }
             return View(list);
             //var q = from p in new 鮮蔬果季Context().Coupons
             //        select p;
             //return View(q);
+
 
         }
         public IActionResult MemberCenter()
