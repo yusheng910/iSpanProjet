08818e65ab2527dddde0d31663d343703c59727f
yusheng910 <79410572+yusheng910@users.noreply.github.com>
2021-12-15 14:13:51 +0800

    OrderController 重建完成

--- 鮮蔬果季_前台/Controllers/OrderController.cs
+++ 鮮蔬果季_前台/Controllers/OrderController.cs
@@ -3,14 +3,63 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using 鮮蔬果季_前台.Models;
+using 鮮蔬果季_前台.ViewModels;
 
 namespace 鮮蔬果季_前台.Controllers
 {
     public class OrderController : Controller
     {
-        public IActionResult Index()
+        public IActionResult Orders()
         {
-            return View();
+            //IEnumerable<Order> orders = null;
+            鮮蔬果季Context db = new 鮮蔬果季Context();
+            List<OrderListViewModel> list = new List<OrderListViewModel>();
+            var orders = (from ord in db.Orders
+                          join stat in db.Statuses
+                          on ord.StatusId equals stat.StatusId
+                          where ord.MemberId == 2
+                          select new { ord, stat });
+
+            db = new 鮮蔬果季Context();
+            foreach (var o in orders)
+            {
+                var 訂單總價 = (from od in db.OrderDetails
+                            join pro in db.Products
+                            on od.ProductId equals pro.ProductId
+                            where od.OrderId == o.ord.OrderId
+                            group new { od, pro } by od.OrderId into g
+                            select g.Sum(p => p.od.UnitsPurchased * p.pro.ProductUnitPrice)).FirstOrDefault();
+                list.Add(new OrderListViewModel() { order = o.ord, status = o.stat, 總價 = 訂單總價 });
+            }
+            return View(list);
+
+        }
+        public IActionResult OrderDetail(int id)
+        {
+            鮮蔬果季Context db = new 鮮蔬果季Context();
+            OrderListViewModel 單筆訂單細項 = new OrderListViewModel();
+            List<OrderListViewModel> 訂單細項列表 = new List<OrderListViewModel>();
+            var 所有訂單細項 = (from od in db.OrderDetails
+                          join p in db.Products
+                          on od.ProductId equals p.ProductId
+                          where od.OrderId == id
+                          select new { od, p });
+            //todo
+            db = new 鮮蔬果季Context();
+            foreach (var o in 所有訂單細項)
+            {
+                var 封面相片 = db.ProductPhotoBanks.Where(p => p.ProductId == o.p.ProductId).FirstOrDefault();
+                訂單細項列表.Add(new OrderListViewModel()
+                {
+                    odetail = o.od,
+                    product = o.p,
+                    photoBank = 封面相片
+                    //單筆訂單細項總價 = 訂單細項總價
+                });
+            }
+
+            return View(訂單細項列表);
         }
     }
 }
