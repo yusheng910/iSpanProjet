3af542133e442fb79eb270981ffa42862e0a197e
LeonLi0317 <isu10122006m@cloud.isu.edu.tw>
2021-12-15 13:24:07 +0800

    更動:移植coupons整體獨立controllers

--- 鮮蔬果季_前台/Controllers/MemberController.cs
+++ 鮮蔬果季_前台/Controllers/MemberController.cs
@@ -75,44 +75,6 @@ namespace 鮮蔬果季_前台.Controllers
         {
             return View();
         }
-        public IActionResult CouponsList()
-        {
-            if (HttpContext.Session.Keys.Contains(CDictionary.SK_LOGINED_USER)) //Seesion有找到
-                ViewBag.USER = UserLogin.member.MemberName;
-            else //Seesion沒找到
-            {
-                ViewBag.USER = null;
-                UserLogin.member = null;
-            }
-            鮮蔬果季Context db = new 鮮蔬果季Context();
-            var qall = from p in db.Coupons
-                       select p;
-            //var q = from p in db.Coupons
-            //        join x in db.CouponDetails
-            //        on p.CouponId equals x.CouponId
-            //        where x.MemberId == 2
-            //        select new { x, p };
-            List<CouponsListViewModel> list = new List<CouponsListViewModel>();
-            foreach (var item in qall)
-            {
-                db = new 鮮蔬果季Context();
-                var q = (from cd in db.CouponDetails
-                         where cd.CouponId == item.CouponId &&
-                         cd.MemberId == UserLogin.member.MemberId
-                         select cd).FirstOrDefault();
-                list.Add(new CouponsListViewModel()
-                {
-                    coupon = item,
-                    couponDetail = q
-                });
-            }
-            return View(list);
-            //var q = from p in new 鮮蔬果季Context().Coupons
-            //        select p;
-            //return View(q);
-
-
-        }
         public IActionResult MemberCenter()
         {
             return View();
