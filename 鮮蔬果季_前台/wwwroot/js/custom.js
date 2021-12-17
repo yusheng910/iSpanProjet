72ce128d66f50801bee98c2fca01e101a7800dbf
Franklyn0602 <95435270+Franklyn0602@users.noreply.github.com>
2021-12-17 22:36:08 +0800

    拔掉一些主控台顯示的錯誤訊息

--- 鮮蔬果季_前台/wwwroot/js/custom.js
+++ 鮮蔬果季_前台/wwwroot/js/custom.js
@@ -210,9 +210,9 @@
 	   NiceScroll
 	   ................................................. */
 
-	$(".brand-box").niceScroll({
-		cursorcolor: "#9b9b9c",
-	});
+	//$(".brand-box").niceScroll({
+	//	cursorcolor: "#9b9b9c",
+	//});
 	
 	
 }(jQuery));
