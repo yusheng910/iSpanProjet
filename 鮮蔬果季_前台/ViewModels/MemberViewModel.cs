74deb12ce76901bdc260df9cb4c28550c4d4e4fb
MinHao-Hsu <95455782+MinHao-Hsu@users.noreply.github.com>
2021-12-17 21:09:40 +0800

    註冊上傳

---
+++ 鮮蔬果季_前台/ViewModels/MemberViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using 鮮蔬果季_前台.Models;
+
+namespace 鮮蔬果季_前台.ViewModels
+{
+    public class MemberViewModel
+    {
+        private  Member _memb = null;
+        public Member member
+        {
+            get
+            {
+                if (_memb == null)
+                    _memb = new Member();
+                return _memb;
+            }
+            set { _memb = value; }
+        }
+        public string  city { get; set; }
+        public int MemberId { get { return this.member.MemberId; } set { this.member.MemberId = value; }  }
+        public string UserId { get {return this.member.UserId; } set {this.member.UserId=value; } }
+        public string Password { get { return this.member.Password; } set {this.member.Password=value; } }
+        public string MemberName { get { return this.member.MemberName; } set { this.member.MemberName = value; } }
+        public string Gender { get { return this.member.Gender; } set { this.member.Gender = value; } }
+        public string MemberAddress { get { return this.member.MemberAddress; } set { this.member.MemberAddress = value; } }
+        public DateTime BirthDate { get { return this.member.BirthDate; } set { this.member.BirthDate = value; } }
+        public string Mobile { get { return this.member.Mobile; } set { this.member.Mobile = value; } }
+        public int CityId { get { return this.member.CityId; } set { this.member.CityId = value; } }
+        public DateTime RegisteredDate { get { return this.member.RegisteredDate; } set { this.member.RegisteredDate = value; } }
+        public string MemberPhotoPass { get { return this.member.MemberPhotoPass; } set { this.member.MemberPhotoPass = value; } }
+    }
+}
