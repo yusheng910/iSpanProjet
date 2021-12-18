ea9a06b57259ab7c985ea5db1ce7f7750409baac
Franklyn0602 <95435270+Franklyn0602@users.noreply.github.com>
2021-12-19 02:37:05 +0800

    layout的CSS設定移動到layoutsetting，如果沒跑出樣板請清除Cookie。 商品List 價錢查尋bar。

--- 鮮蔬果季_前台/wwwroot/js/custom.js
+++ 鮮蔬果季_前台/wwwroot/js/custom.js
@@ -196,8 +196,8 @@
 		$("#slider-range").slider({
 			range: true,
 			min: 0,
-			max: 4000,
-			values: [1000, 3000],
+			max: 1000,
+			values: [250, 750],
 			slide: function(event, ui) {
 				$("#amount").val("$" + ui.values[0] + " - $" + ui.values[1]);
 			}
