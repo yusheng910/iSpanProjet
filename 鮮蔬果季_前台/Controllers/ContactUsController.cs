37f46ba258d5a661ac897eac032818995704447c
Chuewei <89883524+Chuewei@users.noreply.github.com>
2021-12-24 15:10:34 +0800

    15:10 Json

--- 鮮蔬果季_前台/Controllers/ContactUsController.cs
+++ 鮮蔬果季_前台/Controllers/ContactUsController.cs
@@ -3,14 +3,20 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+using System.Linq;
 namespace 鮮蔬果季_前台.Controllers
 {
     public class ContactUsController : Controller
     {
+        
         public IActionResult ContactUs()
         {
             return View();
         }
+        public IActionResult Feebacks()
+        {
+            return View();
+        }
     }
+    
 }
