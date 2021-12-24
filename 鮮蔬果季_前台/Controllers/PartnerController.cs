ed59959b0ac474845969507973bbe4b0f3fff542
dryadl978188 <dryadl978188@gmail.com>
2021-12-24 13:14:06 +0800

    1224 1310 terry 部落格標籤連結   活動首頁分類標籤

--- 鮮蔬果季_前台/Controllers/PartnerController.cs
+++ 鮮蔬果季_前台/Controllers/PartnerController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 using 鮮蔬果季_前台.Models;
@@ -22,6 +23,10 @@ namespace 鮮蔬果季_前台.Controllers
         }
 
 
+
+
+
+
         public IActionResult PartnerBlog(int Id)
         {
 
@@ -38,14 +43,70 @@ namespace 鮮蔬果季_前台.Controllers
             var datas = (from E in db.BlogDetails orderby E.PublishedDate descending
                         select E).ToList();
 
-            if (Id == 1) //目前卡關,想透過前端回傳的id值,來判斷使用哪種LINQ篩選
-            {    //不另外宣告,用原本的datas覆蓋原本查詢 (以下也是foreach也是datas,故無法用新的)
+            if (Id == 1)   //對應標籤進行LINQ查詢
+            {
                  datas = (from E in db.BlogDetails       
                             where E.LabelId == 1
-                         orderby E.PublishedDate descending       //須留意使用orderby會變更資料型態,故這邊也要orderby
+                         orderby E.PublishedDate descending     
                          select E).ToList();
             }
 
+            else if (Id == 2)
+            {    
+                datas = (from E in db.BlogDetails
+                         where E.LabelId == 2
+                         orderby E.PublishedDate descending       
+                         select E).ToList();
+            }
+
+
+            else if (Id == 3)
+            {    
+                datas = (from E in db.BlogDetails
+                         where E.LabelId == 3
+                         orderby E.PublishedDate descending    
+                         select E).ToList();
+            }
+
+            else if (Id == 3)
+            {
+                datas = (from E in db.BlogDetails
+                         where E.LabelId == 3
+                         orderby E.PublishedDate descending
+                         select E).ToList();
+            }
+
+            else if (Id == 11)                 //查詢文章月份  等於 當下月份
+            {
+                datas = (from E in db.BlogDetails
+                         where E.PublishedDate.Value.Month == DateTime.Now.Month
+                         orderby E.PublishedDate descending
+                         select E).ToList();
+            }
+
+            else if (Id == 12)                 //查詢文章月份  等於 當下月份-1
+            {
+                datas = (from E in db.BlogDetails
+                         where E.PublishedDate.Value.Month == DateTime.Now.AddMonths(-1).Month
+                         orderby E.PublishedDate descending
+                         select E).ToList();
+            }
+
+            else if (Id == 13)                 //查詢文章月份  等於 當下月份-2
+            {
+                datas = (from E in db.BlogDetails
+                         where E.PublishedDate.Value.Month == DateTime.Now.AddMonths(-2).Month
+                         orderby E.PublishedDate descending
+                         select E).ToList();
+            }
+
+            else         //卡關,搜索過其他標籤後,再點全部文章,就沒改變
+            { 
+                
+                datas = (from E in db.BlogDetails
+                         orderby E.PublishedDate descending
+                         select E).ToList();
+            }
             List<BlogDetailListViewModel> list = new List<BlogDetailListViewModel>();
             foreach (var item in datas)
             {
@@ -65,42 +126,10 @@ namespace 鮮蔬果季_前台.Controllers
         }
 
 
-        public IActionResult PartnerBlogSelectTag(int id)
-            {
-            // 判斷會員是否登入
-            if (HttpContext.Session.Keys.Contains(CDictionary.SK_LOGINED_USER)) //Seesion有找到
-                ViewBag.USER = UserLogin.member.MemberName;
-            else //Seesion沒找到
-            {
-                ViewBag.USER = null;
-                UserLogin.member = null;
-            }
-
-
-            //鮮蔬果季Context db = new 鮮蔬果季Context();
-            var datas = (from E in db.BlogDetails
-                        where E.LabelId==1
-                        select E).ToList();
-
-                List<BlogDetailListViewModel> list = new List<BlogDetailListViewModel>();
-                foreach (var item in datas)
-                     {
-                    //db = new 鮮蔬果季Context();
-                    var 供應商 = (from Sl in db.Suppliers
-                           where Sl.SupplierId == item.SupplierId
-                           select Sl).FirstOrDefault();
-                    list.Add(new BlogDetailListViewModel()
-                     {
-                         BlogDetail = item,
-                         Supplier = 供應商
-                     });
-                     }
-                return View(list);
-               }
-
 
         public IActionResult PartnerBlog_1(int id)     //此處的id為前台回傳的該農友ID
         {
+
             // 判斷會員是否登入
             if (HttpContext.Session.Keys.Contains(CDictionary.SK_LOGINED_USER)) //Seesion有找到
                 ViewBag.USER = UserLogin.member.MemberName;
@@ -119,16 +148,67 @@ namespace 鮮蔬果季_前台.Controllers
             foreach (var item in datas)
             {
                 //db = new 鮮蔬果季Context();
-                var 供應商 = (from Sl in db.Suppliers
+                var 供應商與城市 = (from Sl in db.Suppliers
+                           join C in db.Cities on Sl.CityId equals C.CityId
                            where Sl.SupplierId == item.SupplierId  
-                           select Sl).FirstOrDefault();
+                           select new { Sl,C}).FirstOrDefault();
                 list.Add(new BlogDetailListViewModel()
                 {
                     BlogDetail = item,
-                    Supplier = 供應商
+                    Supplier = 供應商與城市.Sl,
+                    City = 供應商與城市.C,
                 });
             }
             return View(list);
         }
-    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //public IActionResult PartnerBlogSelectTag(int id)
+        //    {
+        //    // 判斷會員是否登入
+        //    if (HttpContext.Session.Keys.Contains(CDictionary.SK_LOGINED_USER)) //Seesion有找到
+        //        ViewBag.USER = UserLogin.member.MemberName;
+        //    else //Seesion沒找到
+        //    {
+        //        ViewBag.USER = null;
+        //        UserLogin.member = null;
+        //    }
+
+
+        //    //鮮蔬果季Context db = new 鮮蔬果季Context();
+        //    var datas = (from E in db.BlogDetails
+        //                where E.LabelId==1
+        //                select E).ToList();
+
+        //        List<BlogDetailListViewModel> list = new List<BlogDetailListViewModel>();
+        //        foreach (var item in datas)
+        //             {
+        //            //db = new 鮮蔬果季Context();
+        //            var 供應商 = (from Sl in db.Suppliers
+        //                   where Sl.SupplierId == item.SupplierId
+        //                   select Sl).FirstOrDefault();
+        //            list.Add(new BlogDetailListViewModel()
+        //             {
+        //                 BlogDetail = item,
+        //                 Supplier = 供應商
+        //             });
+        //             }
+        //        return View(list);
+        //       }
+         }
 }
