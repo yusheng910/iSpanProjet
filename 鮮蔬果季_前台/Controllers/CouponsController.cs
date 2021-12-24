a07e2c1cc97b6847dc18c91b734c9f84193819eb
LeonLi0317 <isu10122006m@cloud.isu.edu.tw>
2021-12-24 08:58:05 +0800

    更動coupons partial

--- 鮮蔬果季_前台/Controllers/CouponsController.cs
+++ 鮮蔬果季_前台/Controllers/CouponsController.cs
@@ -64,7 +64,7 @@ namespace 鮮蔬果季_前台.Controllers
             return RedirectToAction("CouponsList");
         }
 
-        public IActionResult CouponsListPartial()
+        public IActionResult PartialHavent()
         {
             if (HttpContext.Session.Keys.Contains(CDictionary.SK_LOGINED_USER)) //Seesion有找到
                 ViewBag.USER = UserLogin.member.MemberName;
@@ -73,21 +73,46 @@ namespace 鮮蔬果季_前台.Controllers
                 ViewBag.USER = null;
                 UserLogin.member = null;
             }
-            var qexcept = from c in db.Coupons
-                    join cd in db.CouponDetails
-                    on c.CouponId equals cd.CouponId
-                    where cd.MemberId == UserLogin.member.MemberId
-                    select c.CouponId;
 
+            var qhavent = (from c in db.Coupons
+                           join cd in db.CouponDetails
+                           on c.CouponId equals cd.CouponId
+                           select c).ToList();
 
-
-            var qexcp = from c in db.Coupons
-                          where !qexcept.Contains(c.CouponId)
-                          select c;
-
+            //var qall = (from c in db.Coupons
+            //            where c.CouponId == 3
+            //            && c.CouponId == 5
+            //            select c).ToList();
 
             List<CouponsListViewModel> list = new List<CouponsListViewModel>();
-            foreach (var item in qexcp)
+            foreach (var item in qhavent)
+            {
+                var q = (from cd in db.CouponDetails
+                         where cd.MemberId != UserLogin.member.MemberId
+                         select cd).FirstOrDefault();
+                list.Add(new CouponsListViewModel()
+                {
+                    coupon = item,
+                    couponDetail = q
+                });
+            }
+            return PartialView(list);
+        }
+
+        public IActionResult PartialAll()
+        {
+            if (HttpContext.Session.Keys.Contains(CDictionary.SK_LOGINED_USER)) //Seesion有找到
+                ViewBag.USER = UserLogin.member.MemberName;
+            else //Seesion沒找到
+            {
+                ViewBag.USER = null;
+                UserLogin.member = null;
+            }
+            var qall = (from p in db.Coupons
+                        select p).ToList();
+
+            List<CouponsListViewModel> list = new List<CouponsListViewModel>();
+            foreach (var item in qall)
             {
                 var q = (from cd in db.CouponDetails
                          where cd.CouponId == item.CouponId &&
@@ -100,7 +125,7 @@ namespace 鮮蔬果季_前台.Controllers
                 });
             }
             return PartialView(list);
-
         }
+
     }
 }
