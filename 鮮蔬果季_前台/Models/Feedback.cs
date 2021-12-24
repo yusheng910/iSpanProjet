3d11d376e7b48b351006d57aef95a57e701e3502
dryadl978188 <dryadl978188@gmail.com>
2021-12-24 15:47:53 +0800

    20211224 1550 terry 匯入Entity 報名活動調整

---
+++ 鮮蔬果季_前台/Models/Feedback.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace 鮮蔬果季_前台.Models
+{
+    public partial class Feedback
+    {
+        public int FeedbackId { get; set; }
+        public string FeedbackName { get; set; }
+        public int? FatherFeedbackId { get; set; }
+    }
+}
