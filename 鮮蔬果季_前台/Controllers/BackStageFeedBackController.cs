2ed134219e38543a1f9eebaaaa932b5c528ce807
Chuewei <89883524+Chuewei@users.noreply.github.com>
2021-12-29 17:41:49 +0800

    17:41 回饋表後台管理已建立

--- 鮮蔬果季_前台/Controllers/BackStageFeedBackController.cs
+++ 鮮蔬果季_前台/Controllers/BackStageFeedBackController.cs
@@ -3,14 +3,22 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using 鮮蔬果季_前台.Models;
 
 namespace 鮮蔬果季_前台.Controllers
 {
     public class BackStageFeedBackController : Controller
     {
-        public IActionResult Index()
+        private readonly 鮮蔬果季Context _context;
+        public BackStageFeedBackController(鮮蔬果季Context context)
         {
-            return View();
+           
+            _context = context;
+        }
+        public IActionResult List()
+        {
+            
+            return View(_context.FeedbackResponses);
         }
     }
 }
