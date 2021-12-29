d4dc1478e19285e3a656de8b71d16f0fb85cad6f
dryadl978188 <dryadl978188@gmail.com>
2021-12-29 17:24:50 +0800

    1229 1724 terry 新增blog/event backstage

--- 鮮蔬果季_前台/Controllers/BackstageBlogController.cs
+++ 鮮蔬果季_前台/Controllers/BackstageBlogController.cs
@@ -3,12 +3,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using 鮮蔬果季_前台.ViewModels;
+using 鮮蔬果季_前台.Models;
 
 namespace 鮮蔬果季_前台.Controllers
 {
     public class BackstageBlogController : Controller
     {
-        public IActionResult Index()
+        public IActionResult BlogList()
         {
             return View();
         }
--- 鮮蔬果季_前台/Controllers/BackstageEventController.cs
+++ 鮮蔬果季_前台/Controllers/BackstageEventController.cs
@@ -3,14 +3,43 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using 鮮蔬果季_前台.ViewModels;
+using 鮮蔬果季_前台.Models;
 
 namespace 鮮蔬果季_前台.Controllers
 {
     public class BackstageEventController : Controller
     {
-        public IActionResult Index()
+        private readonly 鮮蔬果季Context db;
+        public BackstageEventController(鮮蔬果季Context dbContext)
         {
-            return View();
+            db = dbContext;
+        }
+
+        public IActionResult EventList()
+        {
+            List<EventListViewModel> 所有活動列表 = new List<EventListViewModel>();
+            var 所有活動 = (from E in db.Events
+                        join supp in db.Suppliers
+                        on E.SupplierId equals supp.SupplierId
+                        select new { E, supp }).ToList();
+
+            foreach (var item in 所有活動)
+            {
+                List<EventPhotoBank> 相片list = new List<EventPhotoBank>();
+                var 城市資料 = db.Cities.FirstOrDefault(C => C.CityId == item.supp.CityId);
+                var 照片資料 = db.EventPhotoBanks.FirstOrDefault(P => P.EventId == item.E.EventId);
+                相片list.Add(照片資料);
+
+                所有活動列表.Add(new EventListViewModel()  //加入EventListViewModel (new新記憶體空間)
+                {
+                    Event = item.E,
+                    City = 城市資料,
+                    EventPhoto = 相片list
+                });
+            }
+            return View(所有活動列表);
+
         }
     }
 }
