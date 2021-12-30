86c5a21b2342b4db52722b01ea641bc214c26b21
Chuewei <89883524+Chuewei@users.noreply.github.com>
2021-12-30 12:09:53 +0800

    12:09 清單美化

--- 鮮蔬果季_前台/Controllers/BackStageFeedBackController.cs
+++ 鮮蔬果季_前台/Controllers/BackStageFeedBackController.cs
@@ -16,11 +16,59 @@ namespace 鮮蔬果季_前台.Controllers
            
             _context = context;
         }
-        public IActionResult List()
+        public IActionResult FeedbackList()
         {
             //List<FeedbackResponseViewModel> 所有意見回饋 = new List<FeedbackResponseViewModel>() ;
             
             return View(_context.FeedbackResponses);
+        }
+        public IActionResult FeedbackCreate()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult FeedbackCreate(FeedbackResponse _response)
+        {
+            FeedbackResponse feedback = new FeedbackResponse()
+            {
+                OrderDetailId = _response.OrderDetailId,
+                FeedbackResponseId = _response.FeedbackResponseId,
+                FeedbackId = _response.FeedbackId,
+                FeedbackComment = _response.FeedbackComment
+            };
+            _context.Add(feedback);
+            _context.SaveChanges();
+            return RedirectToAction("List");
+        }
+        public IActionResult FeedbackDelete(int id)
+        {
+            var message = _context.FeedbackResponses.FirstOrDefault(m => m.FeedbackResponseId == id);
+            if (message != null)
+            {
+                _context.FeedbackResponses.Remove(message);
+                _context.SaveChanges();
+            }
+            return RedirectToAction("List");
+        }
+        public IActionResult FeedbackEdit(int id)
+        {
+            FeedbackResponse message = new 鮮蔬果季Context().FeedbackResponses.FirstOrDefault(m => m.FeedbackResponseId == id);
+            if (message == null)
+                return RedirectToAction("List");
+            return View(message);
+        }
+        [HttpPost]
+        public IActionResult FeedbackEdit(FeedbackResponse _feedback)
+        {
+            FeedbackResponse message = new 鮮蔬果季Context().FeedbackResponses.FirstOrDefault(m => m.FeedbackResponseId == _feedback.FeedbackResponseId);
+            if (message != null)
+            {
+                message.FeedbackResponseId = _feedback.FeedbackResponseId;
+                message.FeedbackId = _feedback.FeedbackId;
+                message.OrderDetailId = _feedback.OrderDetailId;
+                message.FeedbackComment = _feedback.FeedbackComment;
+            }
+            return RedirectToAction("List");
         }
     }
 }
