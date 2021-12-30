192947c797ee7cb38d5faebc2d1d9373bcbbefe7
yusheng910 <79410572+yusheng910@users.noreply.github.com>
2021-12-30 18:48:43 +0800

    Backstage Order update

--- 鮮蔬果季_前台/Controllers/BackstageOrderApiController.cs
+++ 鮮蔬果季_前台/Controllers/BackstageOrderApiController.cs
@@ -3,14 +3,40 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using 鮮蔬果季_前台.Models;
 
 namespace 鮮蔬果季_前台.Controllers
 {
     public class BackstageOrderApiController : Controller
     {
-        public IActionResult Index()
+        private readonly 鮮蔬果季Context db;
+        public BackstageOrderApiController(鮮蔬果季Context dbContext)
         {
-            return View();
+            db = dbContext;
+        }
+        public IActionResult ChangeToShipped(int id)
+        {
+            var 訂單列表 = (from ord in db.Orders
+                       join stat in db.Statuses
+                       on ord.StatusId equals stat.StatusId
+                       select new { ord, stat }).FirstOrDefault();
+            //傳入OrderID 更改送貨狀態 (status) 
+            Order order = db.Orders.FirstOrDefault(i => i.OrderId == id);
+            order.StatusId = 5;
+            db.SaveChanges();
+            
+            return PartialView();
+        }
+        public IActionResult ChangeToDelivered(int id)
+        {
+            var 訂單列表 = (from ord in db.Orders
+                        join stat in db.Statuses
+                        on ord.StatusId equals stat.StatusId
+                        select new { ord, stat }).FirstOrDefault();
+            //傳入OrderID 更改送貨狀態 (status) 
+            Order order = db.Orders.FirstOrDefault(i => i.OrderId == id);
+            order.StatusId = 6;
+            db.SaveChanges(); return PartialView();
         }
     }
 }
