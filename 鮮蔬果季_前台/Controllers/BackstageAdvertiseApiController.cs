5cd2d36ab63dd6199bbeb25821afba104db88d70
Franklyn0602 <95435270+Franklyn0602@users.noreply.github.com>
2022-01-02 14:35:05 +0800

    廣告管理完成

---
+++ 鮮蔬果季_前台/Controllers/BackstageAdvertiseApiController.cs
@@ -0,0 +1,117 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using 鮮蔬果季_前台.Models;
+using 鮮蔬果季_前台.ViewModels;
+
+namespace 鮮蔬果季_前台.Controllers
+{
+    public class BackstageAdvertiseApiController : Controller
+    {
+        private readonly 鮮蔬果季Context db;
+        public BackstageAdvertiseApiController(鮮蔬果季Context dbContext)
+        {
+            db = dbContext;
+        }
+        public IActionResult CateLoad(int id)
+        {
+            List<AdvertiseViewModel> 所有商品列表 = new List<AdvertiseViewModel>();
+            var 所有產品 = (from prod in db.Products
+                        join supp in db.Suppliers
+                        on prod.SupplierId equals supp.SupplierId
+                        join c in db.CategoryDetails
+                        on prod.ProductId equals c.ProductId
+                        where c.CategoryId == id
+                        orderby prod.ProductId
+                        select new { prod, supp }).ToList();
+            foreach (var item in 所有產品)
+            {
+                List<ProductPhotoBank> 相片List = new List<ProductPhotoBank>();
+                var 封面相片 = db.ProductPhotoBanks.FirstOrDefault(p => p.ProductId == item.prod.ProductId);
+                相片List.Add(封面相片);
+                所有商品列表.Add(new AdvertiseViewModel()
+                {
+                    product = item.prod,
+                    supplier = item.supp,
+                    photoBank = 相片List,
+                });
+            }
+
+            return PartialView(所有商品列表);
+        }        
+        public IActionResult AdvertiseLoad(string tag)
+        {
+            List<AdvertiseViewModel> 所有商品列表 = new List<AdvertiseViewModel>();
+            var 所有產品 = (from prod in db.Products
+                        join supp in db.Suppliers
+                        on prod.SupplierId equals supp.SupplierId
+                        join c in db.ProductAdvertises
+                        on prod.ProductId equals c.ProductId
+                        where c.Tag == tag
+                        orderby c.ProductAdvertiseId
+                        select new { prod, supp }).ToList();
+            foreach (var item in 所有產品)
+            {
+                List<ProductPhotoBank> 相片List = new List<ProductPhotoBank>();
+                var 封面相片 = db.ProductPhotoBanks.FirstOrDefault(p => p.ProductId == item.prod.ProductId);
+                相片List.Add(封面相片);
+                所有商品列表.Add(new AdvertiseViewModel()
+                {
+                    product = item.prod,
+                    supplier = item.supp,
+                    photoBank = 相片List,
+                });
+            }
+            return PartialView("CateLoad", 所有商品列表);
+        }       
+        public IActionResult AdvertiseAdd(string pid , string AdvertisSel)
+        {
+            string[] textid = pid.Split(",");
+            int[] productid= Array.ConvertAll<string, int>(textid, s => int.Parse(s)); 
+
+            var 現有廣告商品 = db.ProductAdvertises.Where(a => a.Tag == AdvertisSel).ToList();
+            if (現有廣告商品 != null) {
+                foreach (var item in 現有廣告商品)
+                {
+                    db.Remove(item);
+                }
+                db.SaveChanges();
+            }
+
+            foreach (var id in productid) {
+                ProductAdvertise 新的廣告商品 = new ProductAdvertise()
+                {
+                    ProductId=id,
+                    Tag= AdvertisSel
+                };
+                db.ProductAdvertises.Add(新的廣告商品);
+            }
+            db.SaveChanges();
+
+            List<AdvertiseViewModel> 所有商品列表 = new List<AdvertiseViewModel>();
+            var 所有產品 = (from prod in db.Products
+                        join supp in db.Suppliers
+                        on prod.SupplierId equals supp.SupplierId
+                        join c in db.ProductAdvertises
+                        on prod.ProductId equals c.ProductId
+                        where c.Tag == AdvertisSel
+                        orderby c.ProductAdvertiseId
+                        select new { prod, supp }).ToList();
+            foreach (var item in 所有產品)
+            {
+                List<ProductPhotoBank> 相片List = new List<ProductPhotoBank>();
+                var 封面相片 = db.ProductPhotoBanks.FirstOrDefault(p => p.ProductId == item.prod.ProductId);
+                相片List.Add(封面相片);
+                所有商品列表.Add(new AdvertiseViewModel()
+                {
+                    product = item.prod,
+                    supplier = item.supp,
+                    photoBank = 相片List,
+                });
+            }
+            return PartialView("CateLoad", 所有商品列表);
+        }
+    }
+}
--- 鮮蔬果季_前台/Controllers/BackstageAdvertiseController.cs
+++ 鮮蔬果季_前台/Controllers/BackstageAdvertiseController.cs
@@ -17,14 +17,6 @@ namespace 鮮蔬果季_前台.Controllers
         }
         public IActionResult Advertise()
         {
-            List<AdvertiseViewModel> list = new List<AdvertiseViewModel>();
-            var q = db.ProductAdvertises.ToList();
-            foreach (var item in q) {
-                AdvertiseViewModel advertise = new AdvertiseViewModel()
-                {
-                    productAdvertise = item
-                };
-            }
             List<AdvertiseViewModel> 所有商品列表 = new List<AdvertiseViewModel>();
             var 所有產品 = (from prod in db.Products
                         join supp in db.Suppliers
@@ -48,34 +40,32 @@ namespace 鮮蔬果季_前台.Controllers
             }
             var 商品次類別 = db.Categories.Where(c => c.FatherCategoryId != 8).ToList();
             ViewBag.次類別 = 商品次類別;
-            return View(所有商品列表);
-        }
-        public IActionResult CateLoad(int id)
-        {
-            List<AdvertiseViewModel> 所有商品列表 = new List<AdvertiseViewModel>();
-            var 所有產品 = (from prod in db.Products
+
+            List<AdvertiseViewModel> 廣告區域 = new List<AdvertiseViewModel>();
+            var 所有產品2 = (from prod in db.Products
                         join supp in db.Suppliers
                         on prod.SupplierId equals supp.SupplierId
-                        join c in db.CategoryDetails
+                        join c in db.ProductAdvertises
                         on prod.ProductId equals c.ProductId
-                        where c.CategoryId==id
-                        orderby prod.ProductId
-                        select new { prod, supp }).ToList();
-            foreach (var item in 所有產品)
+                        where c.Tag == "首頁輪播"
+                        orderby c.ProductAdvertiseId
+                         select new { prod, supp }).ToList();
+            foreach (var item in 所有產品2)
             {
                 List<ProductPhotoBank> 相片List = new List<ProductPhotoBank>();
                 var 封面相片 = db.ProductPhotoBanks.FirstOrDefault(p => p.ProductId == item.prod.ProductId);
                 相片List.Add(封面相片);
-                所有商品列表.Add(new AdvertiseViewModel()
+                廣告區域.Add(new AdvertiseViewModel()
                 {
                     product = item.prod,
                     supplier = item.supp,
                     photoBank = 相片List,
                 });
             }
-
-            return PartialView(所有商品列表);
+            ViewBag.首頁輪播商品 = 廣告區域;
+            return View(所有商品列表);
         }
+
 
 
 
