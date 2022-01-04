376d7fdb57066acb3894fce126c5c6067fe56cd2
Chuewei <89883524+Chuewei@users.noreply.github.com>
2022-01-04 16:34:21 +0800

    bug修正

--- 鮮蔬果季_前台/Controllers/LoginController.cs
+++ 鮮蔬果季_前台/Controllers/LoginController.cs
@@ -188,11 +188,16 @@ namespace 鮮蔬果季_前台.Controllers
                             select new {p.UserId ,p.Password }).FirstOrDefault();
             if (ExistuserName != null && ExistuserPassword!=null )
             {
-                if (cust.Password != ExistuserPassword)
+                if (cust != null)
                 {
-                    return Content("1");
+                    if (cust.Password != ExistuserPassword)
+                    {
+                        return Content("1");
+                    }
+                    return Content("2");
                 }
-                return Content("2");
+                return Content("3");
+                
             }
             return Content("0");
         }
