7ea3087555bc80caf0e3c541a07b30517901dd1f
Chuewei <89883524+Chuewei@users.noreply.github.com>
2022-01-10 15:47:04 +0800

    15:36

---
+++ 鮮蔬果季_前台/Controllers/BackStageSupplierController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using 鮮蔬果季_前台.Models;
+
+namespace 鮮蔬果季_前台.Controllers
+{
+    public class BackStageSupplierController : Controller
+    {
+        private readonly 鮮蔬果季Context db;
+        public BackStageSupplierController(鮮蔬果季Context dbContext)
+        {
+            db = dbContext;
+        }
+        public IActionResult SupplierList()
+        {
+            
+            return View();
+        }
+    }
+}
