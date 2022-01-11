0f800ed346110f850df202e6b8c9fa606643c9c9
Chuewei <89883524+Chuewei@users.noreply.github.com>
2022-01-11 17:09:51 +0800

    17:09

--- 鮮蔬果季_前台/Controllers/BackStageSupplierController.cs
+++ 鮮蔬果季_前台/Controllers/BackStageSupplierController.cs
@@ -33,15 +33,82 @@ namespace 鮮蔬果季_前台.Controllers
             }
             return View(list);
         }
+        public IActionResult SupplierListPartial()
+        {
+            List<SupplierViewModel> list = new List<SupplierViewModel>();
+            var suplist = (from sup in db.Suppliers
+                           join cid in db.Cities
+                           on sup.CityId equals cid.CityId
+                           select new { sup, cid }
+                         ).ToList();
+            foreach (var item in suplist)
+            {
+                list.Add(new SupplierViewModel()
+                {
+                    city = item.cid,
+                    supplier = item.sup
+                });
+            }
+            return PartialView(list);
+        }
+
 
         public IActionResult SupplierEditPartail(int id)
         {
-            List<SupplierViewModel> supedit = new List<SupplierViewModel>();
-            var suplist = (
-                from sup in db.Suppliers
-                select new { sup }).ToList();
+
             var suptxt = db.Suppliers.FirstOrDefault(a => a.SupplierId == id);
+            SupplierViewModel supedit = new SupplierViewModel() {
+                supplier = suptxt
+            };
             return PartialView(supedit);
+        }
+        [HttpPost]
+        public IActionResult SupplierEditPartail(SupplierViewModel s)
+        {
+            var supedit = (from a in db.Suppliers
+                           where a.SupplierId==s.SupplierId
+                           select a  
+                           ).FirstOrDefault();
+            //var supcity = (from c in db.Cities
+            //               where c.CityId == s.CityId
+            //              select s).FirstOrDefault();
+            supedit.SupplierAddress = s.SupplierAddress;
+            supedit.BusinessOwner = s.BusinessOwner;
+            supedit.Mobile = s.Mobile;
+            //supcity.CityName = s.CityName;
+            db.SaveChanges();
+            //return RedirectToAction("BackstageSupplier","Supplier");
+            return Content("1");
+        }
+
+        public IActionResult SupplierAddPartial()
+        {
+            var suplist = (from a in db.Suppliers
+                           select  a).FirstOrDefault();
+            List<SupplierViewModel> supadd = new List<SupplierViewModel>();
+            {
+                        
+            };
+            return PartialView();
+        }
+        [HttpPost]
+        public IActionResult SupplierAddPartial(SupplierViewModel sup)
+        {
+            //新增
+            var suplist = (from a in db.Suppliers
+                           join b in db.Cities
+                           on a.CityId equals b.CityId
+                          select new{ a,b}).FirstOrDefault();
+            Supplier supplier = new Supplier()
+            {
+                SupplierName=sup.SupplierName,
+                BusinessOwner=sup.BusinessOwner,
+                SupplierAddress=sup.SupplierAddress,
+                Mobile=sup.Mobile,    
+            }; 
+            db.Add(supplier);
+            db.SaveChanges();
+            return Content();
         }
     }
 }
